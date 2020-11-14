44b674bb5bcbf2dfd85439578363e37bbed2fc23
li-fstz <li-fstz@qq.com>
2020-11-14 23:29:42 +0800

    finish lab2

--- lab2/First.h
+++ lab2/First.h
@@ -32,6 +32,13 @@ typedef struct _Rule {
     struct _Rule *pNextRule;           // 指向下一条文法
 } Rule;
 
+typedef struct _Column {
+    char RuleName[MAX_STR_LENGTH];  // 该非终结符的名称
+    struct _Column *pNextCol;       // 指向下一列
+    int hasVoid;  // 该非终结符是否能推出 ε。1 表示能推出 ε，0 表示不能推出 ε，
+                  // -1 表示未知
+} Column;
+
 typedef struct _Set {
     char Name[MAX_STR_LENGTH];          // 集合的名称
     char Terminal[32][MAX_STR_LENGTH];  // 终结符数组
@@ -47,20 +54,30 @@ typedef struct _SetList {
 // 在此处声明函数
 //
 
-void First(const Rule *pHead, SetList *pFirstSetList);
+void First(const Rule *pHead, Column *pVoidTable, SetList *pFirstSetList);
 Set *GetSet(SetList *pSetList, const char *pName);
 void AddOneSet(SetList *pSetList, const char *pName);
 int AddTerminalToSet(Set *pSet, const char *pTerminal);
 int AddSetToSet(Set *pDesSet, const Set *pSrcSet);
 int SetHasVoid(const Set *pSet);
 
+RuleSymbol *CopySymbol(const RuleSymbol *pSymbolTemplate);
+RuleSymbol *CopySelect(const RuleSymbol *pSelectTemplate);
+Rule *CopyRule(Rule *pHead);
+
+Rule *FindRule(Rule *pHead, const char *RuleName);
+Column *InitVoidTable(Rule *pRule);
+
+void VoidTable(Rule *pRuleHead, Column *pTableHead);
+Column *FindColumn(Column *pTableHead, char *RuleName);
+
 Rule *InitRules();
 Rule *CreateRule(const char *pRuleName);
 RuleSymbol *CreateSymbol();
 
 void PrintRule(const Rule *pHead);
-
 void PrintFirstSet(SetList *pFirstSetList);
+void PrintTable(Column *pHead);
 
 //
 // 在此声明全局变量
--- lab2/main.c
+++ lab2/main.c
@@ -20,11 +20,18 @@ int main(int argc, char *argv[]) {
     PrintRule(pHead);
 
     //
+    // 调用 VoidTable 函数求文法的空表
+    //
+    Column *pVoidTable = InitVoidTable(pHead);
+    VoidTable(pHead, pVoidTable);
+    PrintTable(pVoidTable);
+
+    //
     // 调用 First 函数求文法的 First 集合
     //
     SetList FirstSet;
     FirstSet.nSetCount = 0;
-    First(pHead, &FirstSet);
+    First(pHead, pVoidTable, &FirstSet);
 
     //
     // 输出First集合
@@ -136,13 +143,204 @@ int SetHasVoid(const Set *pSet) {
 
 /*
 功能：
+    求出文法的空表。
+
+参数：
+    pRuleHead -- 文法的头指针。
+    pTableHead -- 空表的头指针。
+*/
+void VoidTable(Rule *pRuleHead, Column *pTableHead) {
+    Rule *pCopiedRule = CopyRule(pRuleHead);
+
+    PrintRule(pCopiedRule);
+    putchar('\n');
+    // Rule *pCopiedRule = pRuleHead;
+    Rule *pRule = pCopiedRule, **pRulePrePtr = &pCopiedRule;
+    while (pRule != NULL) {
+        RuleSymbol *pSelect = pRule->pFirstSymbol,
+                   **pSelectPrePtr = &pRule->pFirstSymbol;
+        int hasVoidSelect = 0;
+        while (pSelect != NULL) {
+            if (pSelect->pNextSymbol == NULL &&
+                strcmp(pSelect->SymbolName, VoidSymbol) == 0) {
+                hasVoidSelect = 1;
+                break;
+            }
+
+            int deleteThisSelect = 0;
+            for (RuleSymbol *pSymble = pSelect; pSymble != NULL;
+                 pSymble = pSymble->pNextSymbol) {
+                if (pSymble->isToken &&
+                    strcmp(pSymble->SymbolName, VoidSymbol) != 0) {
+                    deleteThisSelect = 1;
+                    break;
+                }
+            }
+            if (deleteThisSelect) {
+                *pSelectPrePtr = pSelect->pOther;
+                // FreeSelect(pSelect);
+                pSelect = *pSelectPrePtr;
+            } else {
+                pSelectPrePtr = &pSelect->pOther;
+                pSelect = pSelect->pOther;
+            }
+        }
+        if (hasVoidSelect) {
+            FindColumn(pTableHead, pRule->RuleName)->hasVoid = 1;
+            *pRulePrePtr = pRule->pNextRule;
+            // FreeRule(pRule);
+            pRule = *pRulePrePtr;
+        } else if (pRule->pFirstSymbol == NULL) {
+            FindColumn(pTableHead, pRule->RuleName)->hasVoid = 0;
+            *pRulePrePtr = pRule->pNextRule;
+            // FreeRule(pRule);
+            pRule = *pRulePrePtr;
+        } else {
+            pRulePrePtr = &pRule->pNextRule;
+            pRule = pRule->pNextRule;
+        }
+    }
+    int isChange = 0;
+    do {
+        isChange = 0;
+        Rule *pRule = pCopiedRule, **pRulePrePtr = &pCopiedRule;
+        while (pRule != NULL) {
+            int hasAllNotVoidSelect, hasVoidSelect;
+            for (RuleSymbol *pSelect = pRule->pFirstSymbol; pSelect != NULL;
+                 pSelect = pSelect->pOther) {
+                RuleSymbol *pSymble = pSelect,
+                           **pSymblePrePtr = &pRule->pFirstSymbol;
+                hasAllNotVoidSelect = 1;
+                hasVoidSelect = 0;
+                while (pSymble != NULL) {
+                    switch (
+                        FindColumn(pTableHead, pSymble->SymbolName)->hasVoid) {
+                        case 1:
+                            if (pSymble->pNextSymbol == NULL &&
+                                pSymblePrePtr == &pRule->pFirstSymbol) {
+                                hasVoidSelect = 1;
+                                hasAllNotVoidSelect = 0;
+                            } else {
+                                *pSymblePrePtr = pSymble->pNextSymbol;
+                                pSymble->pNextSymbol->pOther = pSymble->pOther;
+                                // FreeSymble(pSymble);
+                                pSymble = *pSymblePrePtr;
+                            }
+                            break;
+                        case -1:
+                            hasAllNotVoidSelect = 0;
+                        case 0:
+                            pSymblePrePtr = &pSymble->pNextSymbol;
+                            pSymble = pSymble->pNextSymbol;
+                            break;
+                    }
+                    if (hasVoidSelect) {
+                        break;
+                    }
+                }
+                if (hasAllNotVoidSelect || hasVoidSelect) {
+                    break;
+                }
+            }
+            if (hasAllNotVoidSelect || hasVoidSelect) {
+                isChange = 1;
+                FindColumn(pTableHead, pRule->RuleName)->hasVoid =
+                    hasVoidSelect;
+                *pRulePrePtr = pRule->pNextRule;
+                // FreeRule(pRule);
+                pRule = *pRulePrePtr;
+            } else {
+                pRulePrePtr = &pRule->pNextRule;
+                pRule = pRule->pNextRule;
+            }
+        }
+    } while (isChange);
+}
+
+/*
+功能：
+    根据 RuleName 在空表链表中查找名字相同的列。
+
+参数：
+    pTableHead -- 空表链表的头指针。
+    RuleName -- 文法的名字。
+
+返回值：
+    如果存在名字相同的列返回 Column 指针，否则返回 NULL
+*/
+Column *FindColumn(Column *pTableHead, char *RuleName) {
+    for (; pTableHead != NULL; pTableHead = pTableHead->pNextCol) {
+        if (strcmp(pTableHead->RuleName, RuleName) == 0) {
+            return pTableHead;
+        }
+    }
+    return NULL;
+}
+
+Rule *CopyRule(Rule *pHead) {
+    if (pHead == NULL) {
+        return NULL;
+    }
+    Rule *pCopiedHead = CreateRule(pHead->RuleName);
+    if (pHead->pFirstSymbol != NULL) {
+        for (RuleSymbol *pSelect = pHead->pFirstSymbol,
+                        **pSelectPrePtr = &pCopiedHead->pFirstSymbol;
+             pSelect != NULL; pSelect = pSelect->pOther,
+                        pSelectPrePtr = &(*pSelectPrePtr)->pOther) {
+            *pSelectPrePtr = CopySelect(pSelect);
+        }
+    }
+    pCopiedHead->pNextRule = CopyRule(pHead->pNextRule);
+    return pCopiedHead;
+}
+
+/*
+功能：
+        拷贝一个 Symbol。
+
+参数：
+        pSymbolTemplate -- 需要被拷贝的 Symbol 指针。
+
+返回值：
+        拷贝获得的新 Symbol 的指针。
+*/
+RuleSymbol *CopySymbol(const RuleSymbol *pSymbolTemplate) {
+    if (pSymbolTemplate == NULL) {
+        return NULL;
+    }
+    RuleSymbol *pTmp = CreateSymbol();
+    memcpy(pTmp, pSymbolTemplate, sizeof(RuleSymbol));
+    return pTmp;
+}
+
+/*
+功能：
+        拷贝一个 Select。
+
+参数：
+        pSelectTemplate -- 需要被拷贝的 Select 指针。
+
+返回值：
+        拷贝获得的新 Select 的指针。
+*/
+RuleSymbol *CopySelect(const RuleSymbol *pSelectTemplate) {
+    if (pSelectTemplate == NULL) {
+        return NULL;
+    }
+    RuleSymbol *pHead = CopySymbol(pSelectTemplate);
+    pHead->pNextSymbol = CopySelect(pSelectTemplate->pNextSymbol);
+    return pHead;
+}
+
+/*
+功能：
         求文法的 First 集合。
 
 参数：
         pHead -- 文法的头指针。
         pFirstSetList -- First 集合指针。
 */
-void First(const Rule *pHead, SetList *pFirstSetList) {
+void First(const Rule *pHead, Column *pVoidTable, SetList *pFirstSetList) {
     const Rule *pRule;    // Rule 指针
     int isChange;         // 集合是否被修改的标志
     RuleSymbol *pSymbol;  // Symbol 游标
@@ -150,6 +348,10 @@ void First(const Rule *pHead, SetList *pFirstSetList) {
     // 使用文法链表初始化 First 集合
     for (pRule = pHead; pRule != NULL; pRule = pRule->pNextRule) {
         AddOneSet(pFirstSetList, pRule->RuleName);
+        if (FindColumn(pVoidTable, pRule->RuleName)->hasVoid) {
+            AddTerminalToSet(pFirstSetList->Sets + pFirstSetList->nSetCount - 1,
+                             VoidSymbol);
+        }
     }
 
     do {
@@ -158,34 +360,34 @@ void First(const Rule *pHead, SetList *pFirstSetList) {
         for (pRule = pHead; pRule != NULL; pRule = pRule->pNextRule) {
             // 根据文法名称在 pFirstSetList 中查找 Set
             Set *pDesSet = GetSet(pFirstSetList, pRule->RuleName);
+            for (RuleSymbol *pSelect = pRule->pFirstSymbol; pSelect != NULL;
+                 pSelect = pSelect->pOther) {
+                int hasVoid = 1;  // First 集合中是否含有ε的标志
+                for (pSymbol = pSelect; pSymbol != NULL && hasVoid;
+                     pSymbol = pSymbol->pNextSymbol) {
+                    if (pSymbol->isToken)  // 终结符
+                    {
+                        // 调用 AddTerminalToSet 函数将终结符添加到
+                        // pDesSet，并设置修改标志
+                        if (AddTerminalToSet(pDesSet, pSymbol->SymbolName))
+                            isChange = 1;
 
-            int hasVoid = 1;  // First 集合中是否含有ε的标志
-            for (pSymbol = pRule->pFirstSymbol; pSymbol != NULL && hasVoid;
-                 pSymbol = pSymbol->pNextSymbol) {
-                if (pSymbol->isToken)  // 终结符
-                {
-                    // 调用 AddTerminalToSet 函数将终结符添加到
-                    // pDesSet，并设置修改标志
-                    if (AddTerminalToSet(pDesSet, pSymbol->SymbolName))
-                        isChange = 1;
+                        hasVoid = 0;  // 设置 First 集合中是否含有ε的标志
+                    } else            // 非终结符
+                    {
+                        // 根据非终结符名称在 pFirstSetList 中查找 Set
+                        Set *pSrcSet =
+                            GetSet(pFirstSetList, pSymbol->SymbolName);
 
-                    hasVoid = 0;  // 设置 First 集合中是否含有ε的标志
-                } else            // 非终结符
-                {
-                    // 根据非终结符名称在 pFirstSetList 中查找 Set
-                    Set *pSrcSet = GetSet(pFirstSetList, pSymbol->SymbolName);
+                        // 调用 AddSetToSet 函数，将源 Set
+                        // 中的所有终结符添加到目标 Set 中，并设置修改标志
+                        if (AddSetToSet(pDesSet, pSrcSet)) isChange = 1;
 
-                    // 调用 AddSetToSet 函数，将源 Set 中的所有终结符添加到目标
-                    // Set 中，并设置修改标志
-                    if (AddSetToSet(pDesSet, pSrcSet)) isChange = 1;
-
-                    // 调用 SetHasVoid 函数，判断源 Set 中是否含有ε
-                    hasVoid = SetHasVoid(pSrcSet);
+                        // 调用 SetHasVoid 函数，判断源 Set 中是否含有ε
+                        hasVoid =
+                            FindColumn(pVoidTable, pSrcSet->Name)->hasVoid;
+                    }
                 }
-            }
-            if (hasVoid) {
-                // 调用 AddTerminalToSet 函数将ε加入到目标集合中
-                AddTerminalToSet(pDesSet, VoidSymbol);
             }
         }
     } while (isChange);
@@ -193,12 +395,12 @@ void First(const Rule *pHead, SetList *pFirstSetList) {
 
 typedef struct _SYMBOL {
     int isToken;
-    char SymbolName[MAX_STR_LENGTH];
+    char Name[MAX_STR_LENGTH];
 } SYMBOL;
 
 typedef struct _RULE_ENTRY {
     char RuleName[MAX_STR_LENGTH];
-    SYMBOL Symbols[64];
+    SYMBOL Selects[64][64];
 } RULE_ENTRY;
 
 /* exp -> exp addop term| term
@@ -207,15 +409,11 @@ typedef struct _RULE_ENTRY {
    mulop -> *
    factor -> (exp) | number */
 static const RULE_ENTRY rule_table[] = {
-    {"exp", {{0, "exp"}, {0, "addop"}, {0, "term"}}},
-    {"exp", {{0, "term"}}},
-    {"addop", {{1, "+"}}},
-    {"addop", {{1, "-"}}},
-    {"term", {{0, "term"}, {0, "mulop"}, {0, "factor"}}},
-    {"term", {{0, "factor"}}},
-    {"mulop", {{1, "*"}}},
-    {"factor", {{1, "("}, {0, "exp"}, {1, ")"}}},
-    {"factor", {{1, "number"}}}};
+    {"exp", {{{0, "exp"}, {0, "addop"}, {0, "term"}}, {{0, "term"}}}},
+    {"addop", {{{1, "+"}}, {{1, "-"}}}},
+    {"term", {{{0, "term"}, {0, "mulop"}, {0, "factor"}}, {{0, "factor"}}}},
+    {"mulop", {{{1, "*"}}}},
+    {"factor", {{{1, "("}, {0, "exp"}, {1, ")"}}, {{1, "number"}}}}};
 
 /*
 功能：
@@ -226,9 +424,9 @@ static const RULE_ENTRY rule_table[] = {
 */
 Rule *InitRules() {
     Rule *pHead, *pRule;
-    RuleSymbol **pSymbolPtr, *pNewSymbol;
+    RuleSymbol **pSymbolPtr1, **pSymbolPtr2;
     int nRuleCount = sizeof(rule_table) / sizeof(rule_table[0]);
-    int i, j;
+    int i, j, k;
 
     Rule **pRulePtr = &pHead;
     for (i = 0; i < nRuleCount; i++) {
@@ -238,16 +436,28 @@ Rule *InitRules() {
 
     pRule = pHead;
     for (i = 0; i < nRuleCount; i++) {
-        pSymbolPtr = &pRule->pFirstSymbol;
-        for (j = 0; rule_table[i].Symbols[j].SymbolName[0] != '\0'; j++) {
-            const SYMBOL *pSymbol = &rule_table[i].Symbols[j];
+        pSymbolPtr1 = &pRule->pFirstSymbol;
+        for (j = 0; rule_table[i].Selects[j][0].Name[0] != '\0'; j++) {
+            pSymbolPtr2 = pSymbolPtr1;
+            for (k = 0; rule_table[i].Selects[j][k].Name[0] != '\0'; k++) {
+                const SYMBOL *pSymbol = &rule_table[i].Selects[j][k];
 
-            pNewSymbol = CreateSymbol();
-            pNewSymbol->isToken = pSymbol->isToken;
-            strcpy(pNewSymbol->SymbolName, pSymbol->SymbolName);
-            *pSymbolPtr = pNewSymbol;
+                *pSymbolPtr2 = CreateSymbol();
+                (*pSymbolPtr2)->isToken = pSymbol->isToken;
+                strcpy((*pSymbolPtr2)->SymbolName, pSymbol->Name);
+                if (!pSymbol->isToken) {
+                    (*pSymbolPtr2)->pRule = FindRule(pHead, pSymbol->Name);
+                    if (NULL == (*pSymbolPtr2)->pRule) {
+                        printf("Init rules error, miss rule \"%s\"\n",
+                               pSymbol->Name);
+                        exit(1);
+                    }
+                }
 
-            pSymbolPtr = &pNewSymbol->pNextSymbol;
+                pSymbolPtr2 = &(*pSymbolPtr2)->pNextSymbol;
+            }
+
+            pSymbolPtr1 = &(*pSymbolPtr1)->pOther;
         }
 
         pRule = pRule->pNextRule;
@@ -287,8 +497,10 @@ RuleSymbol *CreateSymbol() {
     RuleSymbol *pSymbol = (RuleSymbol *)malloc(sizeof(RuleSymbol));
 
     pSymbol->pNextSymbol = NULL;
+    pSymbol->pOther = NULL;
     pSymbol->isToken = -1;
     pSymbol->SymbolName[0] = '\0';
+    pSymbol->pRule = NULL;
 
     return pSymbol;
 }
@@ -317,6 +529,20 @@ void PrintRule(const Rule *pHead) {
 
 /*
 功能：
+    输出空表。
+
+参数：
+    pHead -- 空表的头指针。
+*/
+void PrintTable(Column *pHead) {
+    if (pHead) {
+        printf("%s\t%d\n", pHead->RuleName, pHead->hasVoid);
+        PrintTable(pHead->pNextCol);
+    }
+}
+
+/*
+功能：
         输出 First 集合。
 
 参数：
@@ -335,4 +561,45 @@ void PrintFirstSet(SetList *pFirstSetList) {
         }
         printf("}\n");
     }
+}
+
+/*
+功能：
+    根据 RuleName 在文法链表中查找名字相同的文法。
+
+参数：
+    pHead -- 文法链表的头指针。
+    RuleName -- 文法的名字。
+
+返回值：
+    如果存在名字相同的文法返回 Rule 指针，否则返回 NULL
+*/
+Rule *FindRule(Rule *pHead, const char *RuleName) {
+    Rule *pRule;
+    for (pRule = pHead; pRule != NULL; pRule = pRule->pNextRule) {
+        if (0 == strcmp(pRule->RuleName, RuleName)) {
+            break;
+        }
+    }
+
+    return pRule;
+}
+
+/*
+功能：
+    初始化空表。
+
+返回值：
+    空表的头指针。
+*/
+Column *InitVoidTable(Rule *pRule) {
+    Column *pHead, **pColumnPrePtr = &pHead;
+    for (; pRule != NULL; pRule = pRule->pNextRule) {
+        *pColumnPrePtr = (Column *)malloc(sizeof(Column));
+        strcpy((*pColumnPrePtr)->RuleName, pRule->RuleName);
+        (*pColumnPrePtr)->hasVoid = -1;
+        (*pColumnPrePtr)->pNextCol = NULL;
+        pColumnPrePtr = &(*pColumnPrePtr)->pNextCol;
+    }
+    return pHead;
 }
